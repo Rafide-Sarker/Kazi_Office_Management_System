a1b5e50f43538954ba9d9204725bc194e1adcf1e
yasinhossain2312080 <2312080@iub.edu.bd>
2026-08-08 20:32:41 +0600

    update Appointment.java

--- src/main/java/com/example/kazi_office_and_marriage_register_office/Appointment.java
+++ src/main/java/com/example/kazi_office_and_marriage_register_office/Appointment.java
@@ -6,6 +6,7 @@ import java.time.LocalTime;
 import java.util.Random;
 
 public class Appointment implements Serializable {
+    private static final long serialVersionUID = 1L;
     private String appointmentId;
     private LocalDate appointmentDate;
     private LocalTime time;
